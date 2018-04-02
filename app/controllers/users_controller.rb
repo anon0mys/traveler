2012213f0f34eef8df993e5bf07e4c02c1a00e55
Evan Wheeler <anon0mys87@gmail.com>
2018-04-02 15:22:01 -0600

    Building sessions and authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = 'Thank you for creating an account!'
       redirect_to user_path(@user)
     else
---
+++ spec/features/users/user_authentication_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+describe 'Authentication' do
+  scenario 'a user can create an account' do
+    visit root_path
+
+    click_on 'Create an Account'
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in 'user[name]', with: 'User Name'
+    fill_in 'user[email]', with: 'user@email.com'
+    fill_in 'user[password]', with: 'password'
+    click_on 'Create Account'
+
+    expect(current_path).to eq user_path(User.last)
+    expect(page).to have_content(User.last.name)
+  end
+
+  scenario 'a user can log in to their account' do
+    user = User.create(name: 'Name', email: 'email@mail.com', password: 'password')
+    visit root_path
+
+    click_on 'Log In'
+
+    expect(current_path).to eq(login_path)
+
+    fill_in 'email', with: user.email
+    fill_in 'password', with: user.password
+    within '.login-form' do
+      click_on 'Log In'
+    end
+
+    expect(current_path).to eq user_path(user)
+    expect(page).to have_content("Welcome, #{user.name}")
+    expect(page).to have_content("Log Out")
+  end
+
+  scenario 'a user can log out of their account' do
+    user = User.create(name: 'Name', email: 'email@mail.com', password: 'password')
+    visit root_path
+    click_on 'Log In'
+
+    fill_in 'email', with: user.email
+    fill_in 'password', with: user.password
+    within '.login-form' do
+      click_on 'Log In'
+    end
+
+    expect(current_path).to eq user_path(user)
+
+    click_on 'Log Out'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to_not have_content("Log Out")
+
+    visit user_path(user)
+
+    expect(page).to_not have_content("Welcome, #{user.name}")
+  end
+end
