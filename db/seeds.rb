12ee9816c082415952fb50c25840c80dcc8aa611
Evan Wheeler <anon0mys87@gmail.com>
2018-04-04 13:20:17 -0600

    Seeds for dev db to start styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+user_one = User.create(name: 'User 1', email: 'user1@mail.com', password: 'password')
+user_two = User.create(name: 'User 2', email: 'user2@mail.com', password: 'password')
+User.create(name: 'Admin', email: 'admin@mail.com', password: 'password', role: 1)
+
+location_one = Location.create(country: 'USA', state: 'Colorado')
+location_two = Location.create(country: 'USA', state: 'California')
+location_three = Location.create(country: 'USA', state: 'Michigan')
+location_four = Location.create(country: 'USA', state: 'Washington')
+
+# User one posts
+(1..6).each do |num|
+  Post.create(title: "Post #{num}", body: 'Body text', user: user_one, location: location_one)
+end
+
+Post.create(title: 'Post 7', body: 'Body text', user: user_one, location: location_two)
+
+(8..11).each do |num|
+  Post.create(title: "Post #{num}", body: 'Body text', user: user_one, location: location_three)
+end
+
+(12..16).each do |num|
+  Post.create(title: "Post #{num}", body: 'Body text', user: user_one, location: location_four)
+end
+
+# User two posts
+Post.create(title: 'Post 17', body: 'Body text', user: user_two, location: location_one)
+
+(18..22).each do |num|
+  Post.create(title: "Post #{num}", body: 'Body text', user: user_two, location: location_two)
+end
+
+(23..25).each do |num|
+  Post.create(title: "Post #{num}", body: 'Body text', user: user_two, location: location_three)
+end
+
+(26..36).each do |num|
+  Post.create(title: "Post #{num}", body: 'Body text', user: user_two, location: location_four)
+end
