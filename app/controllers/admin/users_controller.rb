53b8423008d7d397bdbd41d2b7bb7acd328fd0e4
Evan Wheeler <anon0mys87@gmail.com>
2018-04-04 16:36:07 -0600

    Finished admin auth for users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < Admin::BaseController
   def index
-    @users = User.all
+    @users = User.all.order(:id)
   end
 
   def edit
@@ -8,8 +8,10 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def update
-    user = User.find(params[:id])
-    if user.update(user_params)
+    @user = User.find(params[:id])
+    params[:user].delete(:password) if params[:user][:password].blank?
+    @user.assign_attributes(user_params)
+    if @user.save(validate: false)
       redirect_to admin_users_path
     else
       render :edit
