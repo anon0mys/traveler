210e97e46e172cc7dc4c4c21436af586627be39d
Evan Wheeler <anon0mys87@gmail.com>
2018-04-04 12:50:38 -0600

    Index for users content, with top countries and post count

--- spec/features/anyone/anyone_can_visit_indexes_spec.rb
+++ spec/features/anyone/anyone_can_visit_indexes_spec.rb
@@ -5,9 +5,9 @@ describe 'Anyone' do
     DatabaseCleaner.clean
     @user_one = create(:user)
     @user_two = create(:user)
-    @location = create(:location)
-    @user_one_posts = create_list(:post, 5, user: @user_one, location: @location)
-    @user_two_posts = create_list(:post, 5, user: @user_two, location: @location)
+    @locations = create_list(:location, 5)
+    @user_one_posts = create_list(:post, 5, user: @user_one, location: @locations.first)
+    @user_two_posts = create_list(:post, 5, user: @user_two, location: @locations.first)
   end
 
   after(:each) do
@@ -31,6 +31,27 @@ describe 'Anyone' do
       click_on @user_one_posts.first.title
 
       expect(current_path).to eq(post_path(@user_one_posts.first))
+    end
+  end
+
+  context 'on the users index' do
+    scenario 'should see all users' do
+      visit users_path
+
+      expect(page).to have_content(@user_one.name)
+      expect(page).to have_content(@user_two.name)
+      expect(page).to have_content(@user_one.posts.count)
+      expect(page).to have_content(@user_two.top_three_locations.keys.first)
+    end
+
+    scenario 'should be able to click on users name to go to show page' do
+      visit users_path
+
+      within '.card:first-child' do
+        click_on @user_one.name
+      end
+
+      expect(current_path).to eq(user_path(@user_one))
     end
   end
 end
