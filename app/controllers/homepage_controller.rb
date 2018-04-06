867f0592aeac1a1950df4247b9e21ca89cb651ac
Evan Wheeler <anon0mys87@gmail.com>
2018-04-06 11:52:22 -0600

    New landing page

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,5 +2,6 @@ class HomepageController < ApplicationController
   def index
     @chart_data = Location.sum_of_countries
     @chart_data.unshift(['Country', 'Popularity'])
+    render layout: false
   end
 end
